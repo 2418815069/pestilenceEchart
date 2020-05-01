80bb264f9b345fd4a83144380265fef4f274069b
zyj <1846011454@qq.com>
2020-05-01 18:26:57 +0800

    更新readme文件

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## node爬取数据接口与echarts实现的全国疫情数据分布图
+## node爬取数据接口与ECharts实现的全国疫情数据分布图
 
 ### 运行项目
   + 切换到`server`目录安装相关依赖
@@ -10,3 +10,21 @@
   node app.js
   ```
   + 运行`index.html`
+
+### node爬取数据接口
++ 使用`superagent`和`cheerio`技术
+```js
+// 模拟浏览器向网站发送请求
+const superagent = require("superagent");
+// 服务器端的 jQuery，相当于 html中的 jQuery
+const cheerio = require("cheerio");
+```
++ 爬取步骤如下：
+1. 要爬取的地址：丁香园
+2. 去解析 `html`字符串从里面提取对应的疫情数据
+3. 使用正则表达式，`unescape()`函数转编码,字符串切割或`eval`函数将代码运行
+4. 用 `fs`模块写入数据到本地
+
+### 前端
++ `axios` 发送`ajax`请求
++ `ECharts` 对数据进行可视化渲染
--- server/spider.js
+++ server/spider.js
@@ -15,12 +15,12 @@ superagent.get(url)
     var $getAreaStat = $('#getAreaStat').html();
     var $getListByCountryTypeService2true = $('#getListByCountryTypeService2true').html();
     // console.log($getAreaStat);
-    // 使用正则表达式，unescape()函数转编码,字符串切割或 eval函数将代码运行
+    // 3. 使用正则表达式，unescape()函数转编码,字符串切割或 eval函数将代码运行
     var dataObj = {};  // 定义对象换掉源码的 window
     eval($getAreaStat.replace(/window/g,'dataObj'));
     eval($getListByCountryTypeService2true.replace(/window/g,'dataObj'));
     // console.log(dataObj);  // 此时该对象就有我们添加的属性了
-    // 3. 用 fs模块写入数据到本地
+    // 4. 用 fs模块写入数据到本地
     fs.writeFile(path.join(__dirname,'data.json'),JSON.stringify(dataObj),err=>{
       if(err) throw err;
       console.log('数据写入成功');
